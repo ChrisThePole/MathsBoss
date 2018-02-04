0723eef0b88ce079adf3275f126ad59000a1bc1b
ChrisThePole <32362955+ChrisThePole@users.noreply.github.com>
2018-02-04 18:54:13 +0000

    rfrfrrfr4
    frfefe

--- www/js/index.js
+++ www/js/index.js
@@ -1,18 +1,18 @@
 $(document).on("pagecreate","#pageone",function(){
   
-  	$("#MenuExit").click(function(){
-		exitDialog();				
+  	$("#MenuExit").on("click", (function(){
+		createDialog();				
 	});  
 
 
 });
 
-function exitDialog() {
+/*function exitDialog() {
       
 	navigator.notification.confirm(
     	"Are you sure you want to exit?",  
         dialogDismissed,        
-        "Exit application",            
+        "Exit Application",            
         ['Yes', 'No']                  
     );
 
@@ -28,4 +28,26 @@ function dialogDismissed(buttonIndex) {
 	} else if {
 		(buttonIndex==2) new Toast({content: "Cancelled", duration: 3000});
 
+}*/
+	
+	function createDialog() {
+
+	//phonegap supports native dialog boxes.
+	//here's a simple example
+      
+	navigator.notification.confirm(
+    	"Do you fancy maccies?",  // message
+        dialogDismissed,         // callback
+        "I'm hungry!",            // title
+        ['Of course!', 'No']                  // buttons
+    );
+
 }
+        	
+        	
+        	
+function dialogDismissed(buttonIndex) {
+	
+	if(buttonIndex==1) new Toast({content: "Gotta love maccies!", duration: 3000});
+   	else if(buttonIndex==2) new Toast({content: "I'll get you some selects anyway", duration: 3000});
+
