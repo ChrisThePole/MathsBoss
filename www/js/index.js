c7a532f62847b329959512709f7eb7df61c513c6
ChrisThePole <32362955+ChrisThePole@users.noreply.github.com>
2018-02-15 15:50:11 +0000

    4r4fe
    grfefr

--- www/js/index.js
+++ www/js/index.js
@@ -1,12 +1,13 @@
 $(document).on("pagecreate","#pageone",function(){
   	$("#MenuExit").on("click", (function(){
-		exitDialog();				
+		//exitDialog();
+		window.alert("Menu Exit Button Test");
 	}))  
 })
 //Function that calls the "exitDialog()" function when the "MenuExit" button is pressed
 			
 			   
-function exitDialog() {
+/*function exitDialog() {
 	navigator.notification.confirm(
     	"Are you sure you want to exit?",  
         dialogDismissed,        
@@ -15,7 +16,7 @@ function exitDialog() {
     );
 }
  /*Function that creates a diaglog box asking the user if they definitely want to exit the app, 
- when the "MenuExit" button is pressed*/      	
+ when the "MenuExit" button is pressed      	
         	
         	
 function dialogDismissed(buttonIndex) {
@@ -26,5 +27,5 @@ function dialogDismissed(buttonIndex) {
 		new Toast({content: "Cancelled", duration: 3000});
 }
 }
-/*Function that closes the app if the user confirms they wish to exit
+Function that closes the app if the user confirms they wish to exit
 and displays a toat message which confirms their choice*/
