30bf7c840c980aa505f2157b4acd6c550666cfa6
ChrisThePole <32362955+ChrisThePole@users.noreply.github.com>
2018-02-15 14:38:18 +0000

    Exit Button
    Exit Button

--- www/js/index.js
+++ www/js/index.js
@@ -1,31 +1,30 @@
-/*$(document).on("pagecreate","#pageone",function(){
-  
+$(document).on("pagecreate","#pageone",function(){
   	$("#MenuExit").on("click", (function(){
 		exitDialog();				
-	});  
-
-
-});
-
+	}))  
+})
+//Function that calls the "exitDialog()" function when the "MenuExit" button is pressed
+			
+			   
 function exitDialog() {
-      
 	navigator.notification.confirm(
     	"Are you sure you want to exit?",  
         dialogDismissed,        
         "Exit Application",            
         ['Yes', 'No']                  
     );
-
 }
-        	
+ /*Function that creates a diaglog box asking the user if they definitely want to exit the app, 
+ when the "MenuExit" button is pressed*/      	
         	
         	
 function dialogDismissed(buttonIndex) {
-	
 	if(buttonIndex==1) {
 		window.close;
-		new Toast({content: "Gotta love maccies!", duration: 3000});
-	} else if {
-		(buttonIndex==2) new Toast({content: "Cancelled", duration: 3000});
-
-}*/
+		new Toast({content: "Exit Successful", duration: 3000});
+	} else if (buttonIndex==2) {
+		new Toast({content: "Cancelled", duration: 3000});
+}
+}
+/*Function that closes the app if the user confirms they wish to exit
+and displays a toat message which confirms their choice*/
